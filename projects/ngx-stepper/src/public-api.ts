c62ceb7ebea881900b2ec7dd66ebbfca243451e3
gns088 <kumarganesh088@gmail.com>
2023-08-25 10:15:10 +0530

    prettier

--- projects/ngx-stepper/src/public-api.ts
+++ projects/ngx-stepper/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-stepper
  */
 
-export * from './lib/ngx-stepper.service';
-export * from './lib/ngx-stepper.component';
+export * from './lib/services/ngx-stepper.service';
+export * from './lib/components/ngx-stepper/ngx-stepper.component';
 export * from './lib/ngx-stepper.module';
